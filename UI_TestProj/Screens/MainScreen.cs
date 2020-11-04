b9a2362e06a0c5989c015725346a51e45d9e332d
Anna Demchuk <anidemchuk@gmail.com>
2020-11-04 22:25:22 +0200

    Add Testproject with nunit tests. Test methods Add and Minus

---
+++ UI_TestProj/Screens/MainScreen.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.UITest;
+using Query = System.Func<Xamarin.UITest.Queries.AppQuery, Xamarin.UITest.Queries.AppQuery>;
+
+namespace UI_TestProj.Scrins
+{
+  public  class MainScreen
+    {
+        public Query textField = x => x.Id("input");
+        public Query btnDelete = x => x.Marked("DEL");
+        public Query btnZero = x => x.Marked("0");
+        public Query btnOne = x => x.Marked("1");
+        public Query btnTwo = x => x.Marked("2");
+        public Query btnThree = x => x.Marked("3");
+        public Query btnFour = x => x.Marked("4");
+        public Query btnFive = x => x.Marked("5");
+        public Query btnSix = x => x.Marked("6");
+        public Query btnSeven = x => x.Marked("7");
+        public Query btnEight = x => x.Marked("8");
+        public Query btnNine = x => x.Marked("9");
+
+        public Query btnComma = x => x.Marked(",");
+        public Query btnPlus = x => x.Marked("+");
+        public Query btnMinus = x => x.Marked("-");
+        public Query btnMultiply = x => x.Marked("*,");
+        public Query btnDivide = x => x.Marked("/,");
+        public Query btnEqual = x => x.Marked("=");
+        public static IApp App => AppInitializer.App;
+
+        public static void Repl()
+        {
+            App.Repl();
+        }
+
+
+        public MainScreen TapOnZero()
+        {
+            App.Tap(btnZero);
+            return this;
+        }
+
+
+        public MainScreen TapOnOne()
+        {
+            App.Tap(btnOne);
+            return this;
+        }
+
+
+        public MainScreen TapOnTwo()
+        {
+            App.Tap(btnTwo);
+            return this;
+        }
+
+        public MainScreen TapOnThree()
+        {
+            App.Tap(btnThree);
+            return this;
+        }
+
+        public MainScreen TapOnFour()
+        {
+            App.Tap(btnFour);
+            return this;
+        }
+
+        public MainScreen TapOnFive()
+        {
+            App.Tap(btnFive);
+            return this;
+        }
+
+        public MainScreen TapOnSix()
+        {
+            App.Tap(btnSix);
+            return this;
+        }
+
+        public MainScreen TapOnSeven()
+        {
+            App.Tap(btnSeven);
+            return this;
+        }
+
+        public MainScreen TapOnEight()
+        {
+            App.Tap(btnEight);
+            return this;
+        }
+
+        public MainScreen TapOnNine()
+        {
+            App.Tap(btnNine);
+            return this;
+        }
+
+        public MainScreen TapOnPlus()
+        {
+            App.Tap(btnPlus);
+            return this;
+        }
+
+        public MainScreen TapOnMinus()
+        {
+            App.Tap(btnMinus);
+            return this;
+        }
+
+        public MainScreen TapOnDivide()
+        {
+            App.Tap(btnDivide);
+            return this;
+        }
+
+        public MainScreen TapOnMultply()
+        {
+            App.Tap(btnMultiply);
+            return this;
+        }
+
+        public MainScreen TapOnComma()
+        {
+            App.Tap(btnComma);
+            return this;
+        }
+
+        public MainScreen TapOnDelete()
+        {
+            App.Tap(btnDelete);
+            return this;
+        }
+
+        public MainScreen TapOnEquals()
+        {
+            App.Tap(btnEqual);
+            return this;
+        }
+
+        public string GetTextFromField()
+        {
+            return App.Query(textField)[0].Text.Trim();
+        }
+    }
+
+ 
+}
---
+++ UI_TestProj/TestClass.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using UI_TestProj.Scrins;
+
+namespace UI_TestProj
+{
+    public class TestClass
+    {
+        MainScreen _mainScreen = new MainScreen();
+
+        [SetUp]
+        public void BeforeEach()
+        {
+            AppInitializer.StartApp();
+        }
+
+        //[Test]
+        //public void Test()
+        //{
+        //    //  MainScreen.Repl
+        //    _mainScreen.TapOnOne().TapOnPlus().TapOnTwo().TapOnEquals();
+        //    Assert.AreEqual("3", _mainScreen.GetTextFromField());
+        //}
+
+        [TestCase("3", "4", "7")] //успешно
+        [TestCase("0", "4", "4")] //успешно
+        [TestCase("0", "0", "0")] // System.Exception : Error while performing Query(Id("input"))
+        [TestCase("11", "3", "14")] //успешно
+        [TestCase("20", "5", "25")] // Ошибка (все что имеет ноль- обнуляет)   Expected: "25" But was:  "5"
+        [TestCase("11111111", "22222222", "33333333")]//успешно
+        [TestCase("-3", "8", "5")] //успешно
+        [TestCase("6", "-4", "2")]//успешно
+        [TestCase("-5", "-4", "-9")]//успешно
+        [TestCase("7", "-7", "0")]//успешно
+        [TestCase("-5", "3", "-2")]//успешно
+        public void AddTwoIntegerNumbers(string first, string second, string exectedResult)
+        {
+            GetArrey(first);
+            _mainScreen.TapOnPlus();
+            GetArrey(second);
+            _mainScreen.TapOnEquals();
+
+            Assert.AreEqual(exectedResult, _mainScreen.GetTextFromField());
+        }
+
+        [TestCase("3", "1,3", "4,3")] //успешно
+        [TestCase("1,5", "4", "5,5")] //успешно
+        [TestCase("4", "0,4", "4,4")] // System.Exception : Error while performing Query(Id("input"))
+        [TestCase("0,1", "3", "3,1")] //успешно
+        [TestCase("0,01", "0,002", "0,012")]// System.Exception : Error while performing Query(Id("input"))
+        public void AddTwoDoubleNumbers(string first, string second, string exectedResult)
+        {
+            GetArrey(first);
+            _mainScreen.TapOnPlus();
+            GetArrey(second);
+            _mainScreen.TapOnEquals();
+
+            Assert.AreEqual(exectedResult, _mainScreen.GetTextFromField());
+        }
+
+        [TestCase("0", "0", "0")] // ошибка System.Exception : Error while performing Query(Id("input"))
+        [TestCase("0", "4", "-4")]//успешно
+        [TestCase("5", "0", "0")]// ошибка System.Exception :
+        [TestCase("5", "4", "1")]//успешно
+        [TestCase("5", "8", "-3")]//успешно
+        [TestCase("6", "6", "0")]//успешно
+        [TestCase("50", "5", "45")]//ошибка, округление с нолями -5
+        [TestCase("-5", "4", "-9")] //успешно
+        [TestCase("3", "-2", "5")]//ошибка 1
+        [TestCase("-7", "-1", "-6")]//ошибка -8
+        [TestCase("-1", "-1", "0")]//ошибка -2
+        public void MunusTwoIntegerNumbers(string first, string second, string exectedResult)
+        {
+            GetArrey(first);
+            _mainScreen.TapOnMinus();
+            GetArrey(second);
+            _mainScreen.TapOnEquals();
+
+            Assert.AreEqual(exectedResult, _mainScreen.GetTextFromField());
+        }
+
+
+        [TestCase("3", "1,3", "1,7")]//успешно
+        [TestCase("5,5", "4", "1,5")]//успешно
+        [TestCase("4", "0,4", "3,6")] //System.Exception : Error while performing Query(Id("input"))
+        [TestCase("3", "0,1", "2,9")] //System.Exception : Error while performing Query(Id("input"))
+        [TestCase("0,02", "0,001", "0,019")] //System.Exception : Error while performing Query(Id("input"))
+
+        public void MunusTwoDoubleNumbers(string first, string second, string exectedResult)
+        {
+            GetArrey(first);
+            _mainScreen.TapOnMinus();
+            GetArrey(second);
+            _mainScreen.TapOnEquals();
+
+            Assert.AreEqual(exectedResult, _mainScreen.GetTextFromField());
+        }
+
+
+        public void ClickToNumber(char chars)
+        {
+
+            switch (chars)
+            {
+                case '0':
+                    _mainScreen.TapOnZero();
+                    break;
+                case '1':
+                    _mainScreen.TapOnOne();
+                    break;
+                case '2':
+                    _mainScreen.TapOnTwo();
+                    break;
+                case '3':
+                    _mainScreen.TapOnThree();
+                    break;
+                case '4':
+                    _mainScreen.TapOnFour();
+                    break;
+                case '5':
+                    _mainScreen.TapOnFive();
+                    break;
+                case '6':
+                    _mainScreen.TapOnSix();
+                    break;
+                case '7':
+                    _mainScreen.TapOnSeven();
+                    break;
+                case '8':
+                    _mainScreen.TapOnEight();
+                    break;
+                case '9':
+                    _mainScreen.TapOnNine();
+                    break;
+                case ',':
+                    _mainScreen.TapOnComma();
+                    break;
+                case '-':
+                    _mainScreen.TapOnMinus();
+                    break;
+                case '+':
+                    _mainScreen.TapOnPlus();
+                    break;
+            }
+        }
+
+        public void GetArrey(string number)
+        {
+            char[] chars = number.ToCharArray();
+
+            for (int i = 0; i < chars.Length; i++)
+            {
+                ClickToNumber(chars[i]);
+            }
+
+        }
+    }
+}
